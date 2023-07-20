cc6d0a290490418d2c84b6caf1b30576adfad5c5
Ihor Martynov <igormart80@gmail.com>
2023-07-20 17:48:26 +0300

    Added console application to find the best "execution plan".

--- src/UI/MetaExchangeService.Console/Program.cs
+++ src/UI/MetaExchangeService.Console/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using System.Text.Json;
+using MetaExchangeService.Application;
+using MetaExchangeService.Application.Services;
+using MetaExchangeService.Domain.Models;
+using MetaExchangeService.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(config =>
+    {
+        config.Sources.Clear();
+        config.AddJsonFile("appsettings.json");
+        config.AddCommandLine(Environment.GetCommandLineArgs());
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddRepositories(context.Configuration.GetConnectionString("ExchangeDb")!)
+            .AddApplicationServices();
+    })
+    .ConfigureLogging((_, config) =>
+    {
+        config.SetMinimumLevel(LogLevel.Critical);
+    })
+    .Build();
+
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+if (string.IsNullOrEmpty(configuration["btc-amount"])
+    || string.IsNullOrEmpty(configuration["operation-type"]))
+{
+    Console.WriteLine(@$"The application finds the best execution plan.
+USAGE:
+{Assembly.GetExecutingAssembly().GetName().Name} btc-amount=<amount> operation-type=<buy or sell>");
+}
+
+if (!int.TryParse(configuration["btc-amount"], out var amount)) return;
+
+var executionPlanService = host.Services.GetRequiredService<IExecutionPlanService>();
+
+var executionPlan = configuration["operation-type"]?.ToUpper() switch
+{
+    "BUY" => await executionPlanService.GetBestBuyingPlanAsync(amount, CancellationToken.None),
+    "SELL" => await executionPlanService.GetBestSellingPlanAsync(amount, CancellationToken.None),
+    _ => Enumerable.Empty<ExecutionPlanItem>()
+};
+
+Console.WriteLine("Best execution plan:");
+Console.WriteLine(JsonSerializer.Serialize(executionPlan, new JsonSerializerOptions {WriteIndented = true}));
